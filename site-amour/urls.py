dd81f8265644e20605f24868060189e88385903f
Alexiane <alexiane.laude@mines-paristech.fr>
2019-01-26 10:32:43 +0100

    profil app

--- site-amour/urls.py
+++ site-amour/urls.py
@@ -1,4 +1,4 @@
-"""site_campagne URL Configuration
+"""site-amour URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
--- site-amour/wsgi.py
+++ site-amour/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for site_campagne project.
+WSGI config for site-amour project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_campagne.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site-amour.settings')
 
 application = get_wsgi_application()
