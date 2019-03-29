4aca0db3f7a44f1eaa080b1b48bd5e675f970100
Alexiane <alexiane.laude@mines-paristech.fr>
2019-03-29 13:45:50 +0100

    resol probleme chat

--- chat/forms.py
+++ chat/forms.py
@@ -9,7 +9,7 @@ class NewChat(forms.ModelForm):
 
     def clean_message(self):
         message = self.cleaned_data['message']
-        if 'piche' in message.lower():
+        if message and 'piche' in message.lower():
             raise forms.ValidationError("Piche claquage")
 
         return message  # Ne pas oublier de renvoyer le contenu du champ traité
