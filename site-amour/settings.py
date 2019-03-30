c29e6284883e4042c04958b95bdb9f092ff5015f
Alexiane <alexiane.laude@mines-paristech.fr>
2019-03-30 23:03:25 +0100

    d

--- site-amour/settings.py
+++ site-amour/settings.py
@@ -151,7 +151,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
