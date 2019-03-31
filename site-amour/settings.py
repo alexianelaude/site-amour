33e28c53740e3c1865015178b8bff2ffd701241f
Alexiane <alexiane.laude@mines-paristech.fr>
2019-03-31 01:45:00 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- site-amour/settings.py
+++ site-amour/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '_#$=qp=az5-9x2%4^gjt#4^feu)b8#4jp1-*8r)#03&&tnb#j1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["camino-royale.herokuapp.com"]
+ALLOWED_HOSTS = ["camino-royale.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
