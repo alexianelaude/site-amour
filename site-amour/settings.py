323ad6e53e8f7279e8248c03b89379d55169a187
Alexiane <alexiane.laude@mines-paristech.fr>
2019-03-31 16:42:28 +0200

    l

--- site-amour/settings.py
+++ site-amour/settings.py
@@ -159,7 +159,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
