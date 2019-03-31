7d9a32af0e5523b8f9c09546ab05cb8b5f5e0998
Alexiane <alexiane.laude@mines-paristech.fr>
2019-03-31 14:04:22 +0200

    static

--- site-amour/settings.py
+++ site-amour/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'media'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
