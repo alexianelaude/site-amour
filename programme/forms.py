0a485f3b4e91521f24bb3b92825fe36297e259bb
Elyes <elyes.kaak@gmail.com>
2019-04-01 19:58:04 +0200

    Chat et boite à idées

--- programme/forms.py
+++ programme/forms.py
@@ -4,4 +4,4 @@ class MesureForm(forms.Form):
     comment = forms.CharField(label = 'Ton commentaire:')
 
 class BoiteAIdee(forms.Form):
-    description = forms.CharField(label = "Ton idée ici:")
+    description = forms.CharField(widget = forms.Textarea, label = "Ton idée ici:")
