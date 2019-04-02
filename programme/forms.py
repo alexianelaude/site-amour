c9e788394b0c00ff06725a52ced8fda89a5792c4
Elyes <elyes.kaak@gmail.com>
2019-04-02 11:23:14 +0200

    Vote programme

--- programme/forms.py
+++ programme/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class MesureForm(forms.Form):
-    comment = forms.CharField(label = 'Ton commentaire:')
+    comment = forms.CharField(widget = forms.Textarea, label = 'Ton commentaire:')
 
 class BoiteAIdee(forms.Form):
     description = forms.CharField(widget = forms.Textarea, label = "Ton idée ici:")
