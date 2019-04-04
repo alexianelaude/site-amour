a3f05a660b4a4de058e15addffc141f8fd97308d
Alexiane <alexiane.laude@mines-paristech.fr>
2019-04-04 14:46:59 +0200

    d

--- tetris/admin.py
+++ tetris/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import TetrisModel
 
 # Register your models here.
+class TetrisAdmin(admin.ModelAdmin):
+    list_display = ('score', 'user')
+    list_filter = ('score','user')
 
-admin.site.register(TetrisModel)
+admin.site.register(TetrisModel, TetrisAdmin)
